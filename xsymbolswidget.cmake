162c31d30f71a559893badaddb76c8533b75a40d
horsicq <horsicq@gmail.com>
2024-03-01 00:06:29 +0100

    Update file(s): 2024-03-01

--- xsymbolswidget.cmake
+++ xsymbolswidget.cmake
@@ -5,9 +5,12 @@ include(${CMAKE_CURRENT_LIST_DIR}/../XInfoDB/dialogxinfodbtransferprocess.cmake)
 set(XSYMBOLSWIDGET_SOURCES
     ${DIALOGXINFODBTRANSFERPROCESS_SOURCES}
     ${CMAKE_CURRENT_LIST_DIR}/xsymbolswidget.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/xsymbolswidget.h
     ${CMAKE_CURRENT_LIST_DIR}/xsymbolswidget.ui
     ${CMAKE_CURRENT_LIST_DIR}/dialogbookmarks.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/dialogbookmarks.h
     ${CMAKE_CURRENT_LIST_DIR}/dialogbookmarks.ui
     ${CMAKE_CURRENT_LIST_DIR}/dialogxsymbols.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/dialogxsymbols.h
     ${CMAKE_CURRENT_LIST_DIR}/dialogxsymbols.ui
 )
